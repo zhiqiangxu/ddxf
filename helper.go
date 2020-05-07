ed53200850d23cff9259ef4ca47f73a55fb58510
徐志强 <652732310@qq.com>
2020-05-07 14:39:30 +0800

    add HashObject/HashBytes

--- helper.go
+++ helper.go
@@ -2,6 +2,7 @@ package ddxf
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 
 	"github.com/zhiqiangxu/util"
 )
@@ -12,7 +13,21 @@ func assert(b bool, msg string) {
 	}
 }
 
-func hash(bytes []byte) string {
+// HashObject converts any object to a hash string
+// map keys are sorted asc
+// struct fields are sorted by field order
+// obj: {"f1":"v1",...}
+func HashObject(obj interface{}) (h string, err error) {
+	bytes, err := json.Marshal(obj)
+	if err != nil {
+		return
+	}
+	h = HashBytes(bytes)
+	return
+}
+
+// HashBytes converts any byte sequence to a hash string
+func HashBytes(bytes []byte) string {
 	h := sha256.New()
 	h.Write(bytes)
 	hash := h.Sum(nil)
